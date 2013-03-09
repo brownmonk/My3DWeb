fde69612cf0febde273e45fb86dcee35993617f6
Kendell Joseph <kendelljoseph@gmail.com>
2013-03-09 12:45:55 -0600

    Updated gameboard object, oo la la

--- client/javascript/init.js
+++ client/javascript/init.js
@@ -1,25 +1,48 @@
 var Game = (function (G, $) {
-    var old_client = G.client;  // save old client
-    var old_port = G.clientPort; // save old port
-    var old_gameBoard = G.GameBoard; // save old Game Board
+    if(!$) {
+        console.log('jQuery is not loaded!'); // Abort if Jquery is not loaded first
+        return G;
+    }
 
-    if(!old_client){
+    var old_client = G.client;      // save old client
+    var old_port = G.clientPort;    // save old port
+    var old_gameBoard = G.GameBoard;// save old Game Board
+
+    if(!old_client) {
         if(!old_port){
             G.clientPort = ':4006'; // Default port is arbitrarily 4006
         }
         G.client = io.connect(G.clientPort); // Set up client if client isn't already set
     }
 
-    if(!$) {
-        console.log('jQuery is not loaded!'); // Abort if Jquery is not loaded first
-        return G;
-    }
+    if(!old_gameBoard) {
+        G.GameBoard = {}; // TODO: GameBoard Documentation
 
-    if(!old_gameBoard){
-        G.GameBoard = {}; // TODO: What does the gameboard contain or do?
+        G.GameBoard.Dimentions = {
+            x:8,
+            y:8,
+            tileSize: 80
+        };
+
         G.GameBoard.$ = $('<div></div>') // Dom element attached through "$"
             .attr('id','gameBoard')
+            .css({
+                width:G.GameBoard.Dimentions.tileSize * G.GameBoard.Dimentions.x,
+                height:G.GameBoard.Dimentions.tileSize * G.GameBoard.Dimentions.y
+            })
             .appendTo($('body'));
+
+        G.GameBoard.$.css({
+            top: $(document).height()/2 - G.GameBoard.$.height()/2, // Center gameboard
+            left: $(document).width()/2 - G.GameBoard.$.width()/2
+        });
+
+        $(window).resize(function(){
+            G.GameBoard.$.css({
+                top: $(document).height()/2 - G.GameBoard.$.height()/2, // Center gameboard when window is resized
+                left: $(document).width()/2 - G.GameBoard.$.width()/2
+            });
+        });
     }
 
     $(function () {
@@ -30,4 +53,3 @@ var Game = (function (G, $) {
 
     return G;
 }(Game || {}, jQuery));
-
